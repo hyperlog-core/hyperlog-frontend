2bb8bdc14e8eaed983ad1a8878b3e98bfd8a51c0
Aditya <dtrg21@gmail.com>
2020-05-29 09:34:47 +0530

    Logout user from the server

--- src/helpers/Logout.js
+++ src/helpers/Logout.js
@@ -1,16 +1,32 @@
 import React, { useEffect } from "react";
 import MoonLoader from "react-spinners/MoonLoader";
 import { logoutUser } from "../utils/auth";
-import { useApolloClient } from "@apollo/react-hooks";
+import { useApolloClient, useMutation } from "@apollo/react-hooks";
 import { useHistory } from "react-router-dom";
+import { gql } from "apollo-boost";
 
 const Logout = () => {
   const client = useApolloClient();
   const history = useHistory();
+
+  const LOGOUT_MUTATION = gql`
+    mutation {
+      logout {
+        success
+      }
+    }
+  `;
+
+  const [userLogout] = useMutation(LOGOUT_MUTATION, {
+    onCompleted: () => {
+      logoutUser();
+      client.clearStore();
+      history.push("/login");
+    },
+  });
+
   useEffect(() => {
-    logoutUser();
-    client.clearStore();
-    return history.push("/login");
+    userLogout();
   });
   return (
     <div className="flex h-screen">
