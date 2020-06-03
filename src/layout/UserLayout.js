30c055a847de077309cfae7f51931ed097d1a8be
Aditya <dtrg21@gmail.com>
2020-06-03 16:36:46 +0530

    Add common navbar on all pages

--- src/layout/UserLayout.js
+++ src/layout/UserLayout.js
@@ -4,8 +4,6 @@ import NavBar from "../components/NavBar";
 const UserLayout = (props) => {
   return (
     <>
-      <NavBar />
-
       <header className="bg-white shadow">
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold leading-tight text-gray-900">
