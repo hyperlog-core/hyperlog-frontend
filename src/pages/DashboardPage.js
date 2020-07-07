500e3280926a9b8edb2e2d288fc988cc5dbb0783
Aditya <dtrg21@gmail.com>
2020-07-07 22:40:58 +0530

    Add conditional rendering for analysis section

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -41,11 +41,37 @@ const DashboardPage = () => {
   } else if (!isLoaded) {
     return <div>Loading</div>;
   } else {
-    console.log(profileInfo);
-    return (
-      <UserLayout header={`Dashboard`}>
-        {!githubConnection && <GithubConnect />}
-        {githubConnection && (
+    if (!githubConnection) {
+      return <GithubConnect />;
+    } else if (profileInfo.status === "running") {
+      return (
+        <UserLayout header={`Dashboard`}>
+          <div className="bg-white shadow sm:rounded-lg">
+            <div className="px-4 py-5 sm:p-6">
+              <h3 className="text-lg leading-6 font-medium text-gray-900">
+                We're analysing your profile. It might take a while
+              </h3>
+              <div className="mt-2 max-w-xl text-sm leading-5 text-gray-500">
+                <p>
+                  It seems like this is your first time here. Let's get started
+                  with the first ever analysis of your profile.
+                </p>
+              </div>
+              <div className="mt-5">
+                <button
+                  type="button"
+                  className="inline-flex items-center justify-center px-4 py-2 border border-transparent font-medium rounded-md text-white bg-teal-400 hover:bg-teal-500 focus:outline-none focus:border-teal-200 focus:shadow-outline-teal active:bg-teal-300 active:text-gray-100 transition ease-in-out duration-150 sm:text-sm sm:leading-5"
+                >
+                  Start Analysis
+                </button>
+              </div>
+            </div>
+          </div>
+        </UserLayout>
+      );
+    } else if (profileInfo.turn === 0) {
+      return (
+        <UserLayout header={`Dashboard`}>
           <div className="bg-white shadow sm:rounded-lg">
             <div className="px-4 py-5 sm:p-6">
               <h3 className="text-lg leading-6 font-medium text-gray-900">
@@ -67,9 +93,35 @@ const DashboardPage = () => {
               </div>
             </div>
           </div>
-        )}
-      </UserLayout>
-    );
+        </UserLayout>
+      );
+    } else {
+      return (
+        <UserLayout header={`Dashboard`}>
+          <div className="bg-white shadow sm:rounded-lg">
+            <div className="px-4 py-5 sm:p-6">
+              <h3 className="text-lg leading-6 font-medium text-gray-900">
+                Analyse your profile
+              </h3>
+              <div className="mt-2 max-w-xl text-sm leading-5 text-gray-500">
+                <p>
+                  Have some really cool new contributions/repositories added to
+                  your profile? No worries, let's start the analysis again!
+                </p>
+              </div>
+              <div className="mt-5">
+                <button
+                  type="button"
+                  className="inline-flex items-center justify-center px-4 py-2 border border-transparent font-medium rounded-md text-white bg-teal-400 hover:bg-teal-500 focus:outline-none focus:border-teal-200 focus:shadow-outline-teal active:bg-teal-300 active:text-gray-100 transition ease-in-out duration-150 sm:text-sm sm:leading-5"
+                >
+                  Start Analysis
+                </button>
+              </div>
+            </div>
+          </div>
+        </UserLayout>
+      );
+    }
   }
 };
 
