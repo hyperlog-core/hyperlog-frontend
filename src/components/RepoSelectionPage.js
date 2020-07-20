1336c035c8ceb5df1d4918e1ad0669226850225f
Aditya <dtrg21@gmail.com>
2020-07-20 23:13:16 +0530

    Add selected repositories using selectRepos mutation

--- src/components/RepoSelectionPage.js
+++ src/components/RepoSelectionPage.js
@@ -1,4 +1,8 @@
 import React, { useState } from "react";
+import { gql } from "apollo-boost";
+import { useMutation } from "@apollo/react-hooks";
+import { useHistory } from "react-router-dom";
+import PulseLoader from "react-spinners/PulseLoader";
 
 const IndividualRepo = ({ isSelected, index, repo, onClick }) => {
   const classColors = (languageName) => {
@@ -17,6 +21,7 @@ const IndividualRepo = ({ isSelected, index, repo, onClick }) => {
       case "Python":
         return "bg-indigo-100 text-indigo-800";
       case "Elixir":
+      case "PHP":
         return "bg-purple-100 text-purple-800";
       case "Vue":
         return "bg-green-100 text-green-800";
@@ -31,21 +36,25 @@ const IndividualRepo = ({ isSelected, index, repo, onClick }) => {
     <li className={index === 0 ? "" : "border-t border-gray-200"}>
       <div
         onClick={onClick}
-        class={`block ${
+        className={`block ${
           isSelected
             ? "bg-gray-100 hover:bg-gray-200 focus:outline-none focus:bg-gray-200"
             : "hover:bg-gray-50 focus:outline-none focus:bg-gray-50"
         } transition duration-150 ease-in-out`}
       >
-        <div class="flex items-center px-4 py-4 sm:px-6">
-          <div class="min-w-0 flex-1 flex items-center">
-            <div class="flex-shrink-0">
-              <img class="h-12 w-12 rounded-full" src={repo.imageUrl} alt="" />
+        <div className="flex items-center px-4 py-4 sm:px-6">
+          <div className="min-w-0 flex-1 flex items-center">
+            <div className="flex-shrink-0">
+              <img
+                className="h-12 w-12 rounded-full"
+                src={repo.imageUrl}
+                alt=""
+              />
             </div>
-            <div class="min-w-0 flex-1 px-4 md:grid md:grid-cols-2 md:gap-4">
+            <div className="min-w-0 flex-1 px-4 md:grid md:grid-cols-2 md:gap-4">
               <div>
                 <div
-                  class={`text-sm leading-5 ${
+                  className={`text-sm leading-5 ${
                     isSelected ? "font-bold" : "font-medium"
                   } text-indigo-600 truncate`}
                 >
@@ -53,12 +62,12 @@ const IndividualRepo = ({ isSelected, index, repo, onClick }) => {
                 </div>
                 {repo.description ? (
                   <div
-                    class={`mt-2 flex items-center text-sm leading-5 ${
+                    className={`mt-2 flex items-center text-sm leading-5 ${
                       isSelected ? "text-gray-800 font-medium" : "text-gray-500"
                     }`}
                   >
                     <svg
-                      class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                      className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
                       fill="currentColor"
                       viewBox="0 0 20 20"
                     >
@@ -68,26 +77,26 @@ const IndividualRepo = ({ isSelected, index, repo, onClick }) => {
                         clip-rule="evenodd"
                       ></path>
                     </svg>
-                    <span class="truncate">{repo.description}</span>
+                    <span className="truncate">{repo.description}</span>
                   </div>
                 ) : null}
               </div>
-              <div class="hidden md:block">
+              <div className="hidden md:block">
                 <div>
-                  <div class="text-sm leading-5 text-gray-900">
+                  <div className="text-sm leading-5 text-gray-900">
                     <span
-                      class={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${classColors(
+                      className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${classColors(
                         repo.primaryLanguage
                       )}`}
                     >
                       {repo.primaryLanguage}
                     </span>
                   </div>
-                  <div class="mt-2 flex items-center text-sm leading-5 text-gray-500">
+                  <div className="mt-2 flex items-center text-sm leading-5 text-gray-500">
                     <svg
                       fill="currentColor"
                       viewBox="0 0 20 20"
-                      class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                      className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
                     >
                       <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
                     </svg>
@@ -118,8 +127,33 @@ const IndividualRepo = ({ isSelected, index, repo, onClick }) => {
   );
 };
 
+const MUTATION_SELECT_REPOS = gql`
+  mutation($repos: [String]) {
+    selectRepos(repos: $repos) {
+      success
+      errors
+    }
+  }
+`;
+
 const RepoSelectionPage = ({ repos }) => {
+  const history = useHistory();
   const [selectedPositions, setSelectedPositions] = useState([]);
+  const [mutError, setMutError] = useState(null);
+
+  const [selectRepos, { loading: mutationLoading }] = useMutation(
+    MUTATION_SELECT_REPOS,
+    {
+      onCompleted: (data) => {
+        if (data.selectRepos.success) {
+          history.go();
+        } else {
+          setMutError(data.selectRepos.errors[0]);
+        }
+      },
+      onError: (err) => setMutError(err),
+    }
+  );
 
   const toggleSelection = (i) => {
     if (selectedPositions.includes(i)) {
@@ -133,22 +167,77 @@ const RepoSelectionPage = ({ repos }) => {
     }
   };
 
-  console.log(repos);
+  const submit = () => {
+    const arr = [];
+    selectedPositions.forEach((i) => {
+      arr.push(repos[Object.keys(repos)[i]].full_name);
+    });
+    selectRepos({
+      variables: { repos: arr },
+    });
+  };
 
   return (
-    <ul className="rounded-md py-1 text-base leading-6 shadow-xs overflow-auto focus:outline-none sm:text-sm sm:leading-5">
-      {Object.keys(repos).map((repo, i) => {
-        return (
-          <IndividualRepo
-            key={i}
-            index={i}
-            repo={repos[repo]}
-            isSelected={selectedPositions.includes(i)}
-            onClick={() => toggleSelection(i)}
-          />
-        );
-      })}
-    </ul>
+    <div>
+      {mutError ? (
+        <div className="bg-red-50 border-l-4 border-red-400 p-4">
+          <div className="flex">
+            <div>
+              <p className="text-sm leading-5 text-red-700">{mutError}</p>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="bg-blue-50 border-l-4 border-blue-400 p-4">
+          <div className="flex">
+            <div>
+              <p className="text-sm leading-5 text-blue-700">
+                Hi! Let's just start with selecting 5 repositories that you want
+                to showcase. Be sure to select the ones that really reflect your
+                work and skills!
+              </p>
+            </div>
+          </div>
+        </div>
+      )}
+      <div className="bg-white px-4 py-5 sm:px-6">
+        <div className="-ml-4 -mt-2 flex items-center justify-between flex-wrap sm:flex-no-wrap">
+          <div className="ml-4 mt-2">
+            <h3 className="text-lg leading-6 font-medium text-gray-900">
+              Select 5 repositories
+            </h3>
+          </div>
+          <div className="ml-4 mt-2 flex-shrink-0">
+            <span className="inline-flex rounded-md shadow-sm">
+              <button
+                onClick={() => submit()}
+                type="button"
+                className="relative inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-700 active:bg-indigo-700"
+              >
+                {mutationLoading ? (
+                  <PulseLoader size="10px" color="#ffffff" />
+                ) : (
+                  "Proceed"
+                )}
+              </button>
+            </span>
+          </div>
+        </div>
+      </div>
+      <ul className="rounded-md text-base leading-6 shadow-xs overflow-auto focus:outline-none sm:text-sm sm:leading-5">
+        {Object.keys(repos).map((repo, i) => {
+          return (
+            <IndividualRepo
+              key={i}
+              index={i}
+              repo={repos[repo]}
+              isSelected={selectedPositions.includes(i)}
+              onClick={() => toggleSelection(i)}
+            />
+          );
+        })}
+      </ul>
+    </div>
   );
 };
 
