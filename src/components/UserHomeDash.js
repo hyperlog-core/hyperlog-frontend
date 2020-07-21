e5b8f867131846318afb3abde2a0c26d3993faa7
Aditya <dtrg21@gmail.com>
2020-07-21 13:42:52 +0530

    Remove analyseMutation and add selectRepos instead

--- src/components/UserHomeDash.js
+++ src/components/UserHomeDash.js
@@ -1,11 +1,45 @@
 import React, { useState } from "react";
 import RepoSelectionPage from "./RepoSelectionPage";
 import PulseLoader from "react-spinners/PulseLoader";
+import { useMutation } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
 
-const UserHomeDash = ({ profile, analyse, loading }) => {
+const MUTATION_SELECT_REPOS = gql`
+  mutation($repos: [String]) {
+    selectRepos(repos: $repos) {
+      success
+      errors
+    }
+  }
+`;
+
+const UserHomeDash = ({ profile }) => {
   const [editMode, setEditMode] = useState(false);
+  const [mutError, setMutError] = useState(null);
+
+  const [selectRepos, { loading: mutationLoading }] = useMutation(
+    MUTATION_SELECT_REPOS,
+    {
+      onCompleted: (data) => {
+        if (data.selectRepos.errors) {
+          setMutError(data.selectRepos.errors[0]);
+        }
+      },
+      onError: (err) => setMutError(err),
+    }
+  );
+
   return (
     <div>
+      {mutError ? (
+        <div className="bg-red-50 border-l-4 border-red-400 p-4">
+          <div className="flex">
+            <div>
+              <p className="text-sm leading-5 text-red-700">{mutError}</p>
+            </div>
+          </div>
+        </div>
+      ) : null}
       <div className="bg-white px-4 pb-5 sm:px-6">
         <div className="-ml-4 -mt-2 flex items-center justify-between flex-wrap sm:flex-no-wrap">
           <div className="ml-4 mt-2">
@@ -16,14 +50,18 @@ const UserHomeDash = ({ profile, analyse, loading }) => {
           <div className="ml-4 mt-2 flex-shrink-0">
             <span className="inline-flex rounded-md shadow-sm">
               <button
-                onClick={() => analyse()}
+                onClick={() =>
+                  selectRepos({
+                    variables: { repos: profile.selectedRepos },
+                  })
+                }
                 type="button"
                 className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150"
               >
-                {loading ? (
+                {mutationLoading ? (
                   <PulseLoader size="10px" color="#161e2e" />
                 ) : (
-                  "Analyse Again"
+                  "Analyse Repos Again"
                 )}
               </button>
             </span>
--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -3,19 +3,8 @@ import UserLayout from "../layout/UserLayout";
 import GithubConnect from "../components/GithubConnect";
 import { useRecoilValue, useRecoilValueLoadable } from "recoil";
 import { isGithubConnected, currentUser } from "../store/atoms";
-import { gql } from "apollo-boost";
-import { useMutation } from "@apollo/react-hooks";
 import ProfileInfo from "../components/ProfileInfo";
 import PulseLoader from "react-spinners/PulseLoader";
-
-const MUTATION_ANALYSE_USER_PROFILE = gql`
-  mutation {
-    analyseProfile {
-      success
-      errors
-    }
-  }
-`;
 
 const fetchUserData = (userID) => {
   return fetch(`${process.env.REACT_APP_AWS_URL}/user/${userID}`).then((res) =>
@@ -25,40 +14,11 @@ const fetchUserData = (userID) => {
 
 const DashboardPage = () => {
   const [error, setError] = useState(null);
-  const [mutError, setMutError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [profileInfo, setProfileInfo] = useState({});
 
   const githubConnection = useRecoilValue(isGithubConnected);
   const user = useRecoilValueLoadable(currentUser);
-
-  const [
-    startAnalysis,
-    { loading: mutationLoading, error: mutationError },
-  ] = useMutation(MUTATION_ANALYSE_USER_PROFILE, {
-    onCompleted: (data) => {
-      if (data.analyseProfile.success) {
-        setProfileInfo({
-          ...profileInfo,
-          status: "running",
-        });
-        setTimeout(
-          fetchUserData(user.contents.user.id).then(
-            (result) => {
-              setProfileInfo(result);
-            },
-            (error) => {
-              setError(error);
-            }
-          ),
-          2000
-        );
-      } else {
-        setMutError(data.analyseProfile.errors[0]);
-      }
-    },
-    onError: (err) => console.log(err),
-  });
 
   switch (user.state) {
     case "hasValue":
@@ -98,33 +58,7 @@ const DashboardPage = () => {
     } else {
       return (
         <UserLayout header={`Dashboard`}>
-          {(mutationError || mutError) && (
-            <div className="bg-red-50 border-l-4 border-red-400 p-4 mb-2">
-              <div className="flex">
-                <div className="flex-shrink-0">
-                  <svg
-                    className="h-5 w-5 text-red-400"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </div>
-                <div className="ml-3">
-                  <p className="text-sm leading-5 text-red-700">{mutError}</p>
-                </div>
-              </div>
-            </div>
-          )}
-          <ProfileInfo
-            profile={profileInfo}
-            analyse={startAnalysis}
-            loading={mutationLoading}
-          />
+          <ProfileInfo profile={profileInfo} />
         </UserLayout>
       );
     }
