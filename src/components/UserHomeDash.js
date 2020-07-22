4b9188abf414b2db82323c1c5308217092dc42ec
Aditya <dtrg21@gmail.com>
2020-07-22 12:53:25 +0530

    Fix submitting empty repos array

--- src/components/UserHomeDash.js
+++ src/components/UserHomeDash.js
@@ -40,43 +40,45 @@ const UserHomeDash = ({ profile }) => {
           </div>
         </div>
       ) : null}
-      <div className="bg-white px-4 pb-5 sm:px-6">
-        <div className="-ml-4 -mt-2 flex items-center justify-between flex-wrap sm:flex-no-wrap">
-          <div className="ml-4 mt-2">
-            <h3 className="text-lg leading-6 font-medium text-gray-900">
-              Your showcase
-            </h3>
-          </div>
-          <div className="ml-4 mt-2 flex-shrink-0">
-            <span className="inline-flex rounded-md shadow-sm">
-              <button
-                onClick={() =>
-                  selectRepos({
-                    variables: { repos: profile.selectedRepos },
-                  })
-                }
-                type="button"
-                className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150"
-              >
-                {mutationLoading ? (
-                  <PulseLoader size="10px" color="#161e2e" />
-                ) : (
-                  "Analyse Repos Again"
-                )}
-              </button>
-            </span>
-            <span className="inline-flex rounded-md shadow-sm ml-2">
-              <button
-                onClick={() => setEditMode(!editMode)}
-                type="button"
-                className="relative inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-700 active:bg-indigo-700 transition ease-in-out duration-150"
-              >
-                Edit
-              </button>
-            </span>
+      {!editMode && (
+        <div className="bg-white px-4 pb-5 sm:px-6">
+          <div className="-ml-4 -mt-2 flex items-center justify-between flex-wrap sm:flex-no-wrap">
+            <div className="ml-4 mt-2">
+              <h3 className="text-lg leading-6 font-medium text-gray-900">
+                Your showcase
+              </h3>
+            </div>
+            <div className="ml-4 mt-2 flex-shrink-0">
+              <span className="inline-flex rounded-md shadow-sm">
+                <button
+                  onClick={() =>
+                    selectRepos({
+                      variables: { repos: profile.selectedRepos },
+                    })
+                  }
+                  type="button"
+                  className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150"
+                >
+                  {mutationLoading ? (
+                    <PulseLoader size="10px" color="#161e2e" />
+                  ) : (
+                    "Analyse Repos Again"
+                  )}
+                </button>
+              </span>
+              <span className="inline-flex rounded-md shadow-sm ml-2">
+                <button
+                  onClick={() => setEditMode(!editMode)}
+                  type="button"
+                  className="relative inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-700 active:bg-indigo-700 transition ease-in-out duration-150"
+                >
+                  Edit
+                </button>
+              </span>
+            </div>
           </div>
         </div>
-      </div>
+      )}
       <RepoSelectionPage
         repos={profile.repos}
         selected={profile.selectedRepos ?? []}
