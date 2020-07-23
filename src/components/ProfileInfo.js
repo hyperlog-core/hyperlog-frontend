9c010d20a65daff8fd9784da68b73bca33b34c04
Aditya <dtrg21@gmail.com>
2020-07-23 10:53:21 +0530

    Fix fetching before github connection

--- src/components/ProfileInfo.js
+++ src/components/ProfileInfo.js
@@ -1,21 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import PulseLoader from "react-spinners/PulseLoader";
 import RepoSelectionPage from "./RepoSelectionPage";
 import UserHomeDash from "./UserHomeDash";
+import { useRecoilValueLoadable } from "recoil";
+import { currentUser } from "../store/atoms";
 
-const ProfileInfo = ({ profile, analyse, loading }) => {
-  if (!profile.selectedRepos) {
+const fetchUserData = (userID) => {
+  return fetch(`${process.env.REACT_APP_AWS_URL}/user/${userID}`).then((res) =>
+    res.json()
+  );
+};
+
+const ProfileInfo = ({ analyse, loading }) => {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [profileInfo, setProfileInfo] = useState({});
+  const [calledOnce, setCalledOnce] = useState(false);
+
+  const user = useRecoilValueLoadable(currentUser);
+
+  switch (user.state) {
+    case "hasValue":
+      if (user.contents.user && !isLoaded && !calledOnce) {
+        setCalledOnce(true);
+        fetchUserData(user.contents.user.id).then(
+          (result) => {
+            setProfileInfo(result);
+            setIsLoaded(true);
+          },
+          (error) => {
+            setError(error);
+            setIsLoaded(true);
+          }
+        );
+      }
+      break;
+    case "hasError":
+    default:
+      throw user.contents;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return (
+      <div className="flex w-full h-64 justify-center items-center">
+        <PulseLoader size="20px" color="#374151" />
+      </div>
+    );
+  } else if (!profileInfo.selectedRepos) {
     return (
       <RepoSelectionPage
-        repos={profile.repos}
-        selected={profile.selectedRepos ?? []}
+        repos={profileInfo.repos}
+        selected={profileInfo.selectedRepos ?? []}
         editMode={true}
         firstTime={true}
       />
     );
-  } else if (profile.selectedRepos) {
+  } else if (profileInfo.selectedRepos) {
     return (
-      <UserHomeDash profile={profile} analyse={analyse} loading={loading} />
+      <UserHomeDash profile={profileInfo} analyse={analyse} loading={loading} />
     );
   } else {
     return (
--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,76 +1,25 @@
-import React, { useState } from "react";
+import React from "react";
 import UserLayout from "../layout/UserLayout";
 import GithubConnect from "../components/GithubConnect";
-import { useRecoilValue, useRecoilValueLoadable } from "recoil";
-import { isGithubConnected, currentUser } from "../store/atoms";
+import { useRecoilValue } from "recoil";
+import { isGithubConnected } from "../store/atoms";
 import ProfileInfo from "../components/ProfileInfo";
-import PulseLoader from "react-spinners/PulseLoader";
-import { useHistory } from "react-router-dom";
-
-const fetchUserData = (userID) => {
-  return fetch(`${process.env.REACT_APP_AWS_URL}/user/${userID}`).then((res) =>
-    res.json()
-  );
-};
 
 const DashboardPage = () => {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [profileInfo, setProfileInfo] = useState({});
-  const [calledOnce, setCalledOnce] = useState(false);
-
-  const history = useHistory();
-
   const githubConnection = useRecoilValue(isGithubConnected);
-  const user = useRecoilValueLoadable(currentUser);
 
-  switch (user.state) {
-    case "hasValue":
-      if (user.contents.user && !isLoaded && !calledOnce) {
-        setCalledOnce(true);
-        fetchUserData(user.contents.user.id).then(
-          (result) => {
-            if (result) {
-              setProfileInfo(result);
-            } else {
-              history.go();
-            }
-            setIsLoaded(true);
-          },
-          (error) => {
-            setError(error);
-            setIsLoaded(true);
-          }
-        );
-      }
-      break;
-    case "hasError":
-    default:
-      throw user.contents;
-  }
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  } else if (!isLoaded) {
+  if (!githubConnection) {
     return (
-      <div className="flex w-full h-64 justify-center items-center">
-        <PulseLoader size="20px" color="#374151" />
-      </div>
+      <UserLayout header={`One more step...`}>
+        <GithubConnect />
+      </UserLayout>
     );
   } else {
-    if (!githubConnection) {
-      return (
-        <UserLayout header={`Dashboard`}>
-          <GithubConnect />
-        </UserLayout>
-      );
-    } else {
-      return (
-        <UserLayout header={`Dashboard`}>
-          <ProfileInfo profile={profileInfo} />
-        </UserLayout>
-      );
-    }
+    return (
+      <UserLayout header={`Dashboard`}>
+        <ProfileInfo />
+      </UserLayout>
+    );
   }
 };
 
