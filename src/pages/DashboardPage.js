c7dbd0bb28b39a81b1c5ff5a439e2a1f85f6b1c5
Aditya <dtrg21@gmail.com>
2020-07-26 12:29:38 +0530

    Fix error on user registration

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -4,6 +4,7 @@ import GithubConnect from "../components/GithubConnect";
 import { useRecoilValue } from "recoil";
 import { isGithubConnected } from "../store/atoms";
 import ProfileInfo from "../components/ProfileInfo";
+import PulseLoader from "react-spinners/PulseLoader";
 
 const DashboardPage = () => {
   const githubConnection = useRecoilValue(isGithubConnected);
@@ -14,10 +15,16 @@ const DashboardPage = () => {
         <GithubConnect />
       </UserLayout>
     );
-  } else {
+  } else if (githubConnection) {
     return (
       <UserLayout header={`Dashboard`}>
         <ProfileInfo />
+      </UserLayout>
+    );
+  } else {
+    return (
+      <UserLayout header={`Loading...`}>
+        <PulseLoader size="60px" color="#000000" />
       </UserLayout>
     );
   }
