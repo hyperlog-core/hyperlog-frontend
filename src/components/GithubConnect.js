83511178768eb6d707ce04913b729fe36b347d24
Aditya <dtrg21@gmail.com>
2020-08-09 18:10:46 +0530

    Improve modal with animations and common message

--- src/components/GithubConnect.js
+++ src/components/GithubConnect.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import Portal from "../Portal";
 import github from "../github-logo.svg";
 import exclamation from "../exclamation.svg";
+import Transition from "../helpers/Transition";
 
 const connect_github = (scope) => {
   window.open(
@@ -13,67 +14,110 @@ const connect_github = (scope) => {
   );
 };
 
-class GithubConnect extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showModal: false,
-    };
-
-    this.handleShow = this.handleShow.bind(this);
-    this.handleHide = this.handleHide.bind(this);
-  }
-
-  handleShow() {
-    this.setState({ showModal: true });
-  }
-
-  handleHide() {
-    this.setState({ showModal: false });
-  }
-
-  render() {
-    const modal = this.state.showModal ? (
-      <Portal>
+const ScopeSelection = ({ isOpen, setIsOpen }) => {
+  return (
+    <div
+      className={
+        isOpen
+          ? "fixed bottom-0 inset-x-0 px-4 pb-4 sm:inset-0 sm:flex sm:items-center sm:justify-center"
+          : ""
+      }
+    >
+      <Transition
+        show={isOpen}
+        enter="ease-out duration-300"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="ease-in duration-200"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
         <div
-          className="modal fixed bg-black bg-opacity-50 w-full h-full top-0 left-0 flex items-center justify-center"
-          onClick={this.handleHide}
+          className="fixed inset-0 transition-opacity"
+          onClick={() => setIsOpen(false)}
         >
-          <div className="w-3/12 rounded-lg overflow-hidden shadow-lg bg-white">
-            <p>
-              <button
-                className="w-full height-auto hover:bg-purple-800"
-                onClick={() => connect_github("public")}
+          <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+        </div>
+      </Transition>
+      <Transition
+        show={isOpen}
+        enter="ease-out duration-300"
+        enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+        enterTo="opacity-100 translate-y-0 sm:scale-100"
+        leave="ease-in duration-200"
+        leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+        leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+      >
+        <div
+          className="bg-white rounded-lg px-4 pt-5 pb-4 overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full sm:p-6"
+          role="dialog"
+          aria-modal="true"
+          aria-labelledby="modal-headline"
+        >
+          <div className="sm:flex sm:items-start">
+            <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-yellow-100 sm:mx-0 sm:h-10 sm:w-10">
+              <svg
+                className="h-6 w-6 text-yellow-600"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
               >
-                <strong>Only public repositories</strong>
-                <span className="text-sm">
-                  <p>
-                    Allow access to only public repositories you've contributed
-                    to
-                  </p>
-                </span>
-              </button>
-            </p>
-            <p>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+                />
+              </svg>
+            </div>
+            <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+              <h3
+                className="text-lg leading-6 font-medium text-gray-900"
+                id="modal-headline"
+              >
+                Repo access level
+              </h3>
+              <div className="mt-2">
+                <p className="text-sm leading-5 text-gray-500">
+                  Due to GitHub's API limitations, we ask for both read and
+                  write access to the repositories, but{" "}
+                  <b>we never use the write access without explicit consent</b>{" "}
+                  from you. So what level of access do you want us to have to
+                  your repositories?
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="mt-5 sm:mt-4 sm:ml-10 sm:pl-4 sm:flex">
+            <span className="flex w-full rounded-md shadow-sm sm:w-auto">
               <button
-                className="w-full height-auto hover:bg-purple-800"
+                type="button"
+                className="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-indigo-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition ease-in-out duration-150 sm:text-sm sm:leading-5"
                 onClick={() => connect_github("full")}
               >
-                <strong>All repositories</strong>
-                <span className="text-sm">
-                  <p>
-                    Allow access to both public and private repositories you've
-                    contributed to
-                  </p>
-                </span>
+                Private + Public Repos
               </button>
-            </p>
+            </span>
+            <span className="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:ml-3 sm:w-auto">
+              <button
+                type="button"
+                className="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5"
+                onClick={() => connect_github("public")}
+              >
+                Only Public Repos
+              </button>
+            </span>
           </div>
         </div>
-      </Portal>
-    ) : null;
+      </Transition>
+    </div>
+  );
+};
 
-    return (
+const GithubConnect = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
       <div className="bg-white shadow sm:rounded-lg">
         <div className="px-4 py-5 sm:p-6">
           <h3 className="inline-flex text-lg leading-6 font-medium text-gray-900">
@@ -92,21 +136,25 @@ class GithubConnect extends React.Component {
             <div className="mt-5 sm:mt-0 sm:ml-6 sm:flex-shrink-0 sm:flex sm:items-center">
               <span className="inline-flex rounded-md shadow-sm">
                 <button
-                  onClick={this.handleShow}
+                  onClick={() => {
+                    setIsOpen(true);
+                  }}
                   type="button"
                   className="inline-flex items-center px-4 py-2 border border-gray-300 text-base leading-6 font-medium rounded-md text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150"
                 >
                   <img src={github} alt="GitHub logo" />
                   &nbsp;Connect with GitHub
                 </button>
-                {modal}
               </span>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+      <Portal>
+        <ScopeSelection isOpen={isOpen} setIsOpen={setIsOpen} />
+      </Portal>
+    </>
+  );
+};
 
 export default GithubConnect;
