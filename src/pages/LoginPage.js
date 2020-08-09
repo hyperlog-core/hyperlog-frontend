a325ea6e6f2e3a31778e6cae6c1fc784c822748b
Aditya <dtrg21@gmail.com>
2020-08-09 22:11:12 +0530

    Add similar logic to login page

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -9,6 +9,7 @@ import { loginUser } from "../utils/auth";
 import PulseLoader from "react-spinners/PulseLoader";
 import { useSetRecoilState } from "recoil";
 import { currentUser } from "../store/atoms";
+import GitHubLogin from "react-github-login";
 
 const LoginPage = () => {
   const MUTATION_LOGIN_USER = gql`
@@ -25,9 +26,42 @@ const LoginPage = () => {
     }
   `;
 
+  const MUTATION_LOGIN_GITHUB = gql`
+    mutation($code: String!) {
+      loginWithGithub(code: $code) {
+        success
+        errors
+        login {
+          token
+          user {
+            id
+            email
+            firstName
+            lastName
+          }
+        }
+      }
+    }
+  `;
+
   let history = useHistory();
 
   const setUser = useSetRecoilState(currentUser);
+
+  const [loginWithGithub, { loading: ghLoading }] = useMutation(
+    MUTATION_LOGIN_GITHUB,
+    {
+      onCompleted: (data) => {
+        loginUser(data.loginWithGithub.login.token, false);
+        setUser({
+          loggedIn: true,
+          user: data.loginWithGithub.login.user,
+        });
+        history.push("/dashboard");
+      },
+      onError: (err) => console.log(err),
+    }
+  );
 
   const [
     userLogin,
@@ -79,6 +113,53 @@ const LoginPage = () => {
           </div>
 
           <div className="mt-8">
+            <div>
+              <div>
+                <span class="w-full inline-flex rounded-md shadow-sm">
+                  <GitHubLogin
+                    clientId="42782b0ad960d7bae699"
+                    redirectUri=""
+                    className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md bg-white text-sm leading-5 font-medium text-gray-500 hover:text-gray-400 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition duration-150 ease-in-out"
+                    onSuccess={(data) =>
+                      loginWithGithub({ variables: { code: data["code"] } })
+                    }
+                    onFailure={(e) => console.log(e)}
+                    children={
+                      ghLoading ? (
+                        <PulseLoader size="10px" />
+                      ) : (
+                        <>
+                          <svg
+                            className="-ml-1 mr-3 w-5 h-5"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z"
+                              clipRule="evenodd"
+                            />
+                          </svg>
+                          Login with Github
+                        </>
+                      )
+                    }
+                  />
+                </span>
+              </div>
+
+              <div class="mt-6 relative">
+                <div class="absolute inset-0 flex items-center">
+                  <div class="w-full border-t border-gray-300"></div>
+                </div>
+                <div class="relative flex justify-center text-sm leading-5">
+                  <span class="px-2 bg-white text-gray-500">
+                    Or continue with
+                  </span>
+                </div>
+              </div>
+            </div>
+
             <div className="mt-6">
               {mutationError ? (
                 <div className="rounded-md bg-red-50 p-4 mb-6">
