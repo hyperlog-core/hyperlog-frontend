1d956eb1bfcc0a818657b1f5bb788d07a7b380dc
Aditya <dtrg21@gmail.com>
2020-08-11 23:36:06 +0530

    Add better image for reflecting the Hyperlog brand

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -338,7 +338,7 @@ const LoginPage = () => {
       <div className="hidden lg:block relative w-0 flex-1">
         <img
           className="absolute inset-0 h-full w-full object-cover"
-          src="https://images.unsplash.com/photo-1505904267569-f02eaeb45a4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
+          src="https://images.unsplash.com/photo-1528660544347-95a93c58e424?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=steve-halama-Yhc7YGZlz3g-unsplash.jpg&w=1920"
           alt=""
         />
       </div>
