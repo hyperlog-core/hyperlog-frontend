c40cf058adda99b175a822e3acf8ce2ca092a456
Aditya <dtrg21@gmail.com>
2020-08-13 18:01:16 +0530

    Don't allow user to change the email address

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -14,14 +14,13 @@ const GET_CURRENT_USER_QUERY = gql`
     thisUser {
       firstName
       lastName
-      email
     }
   }
 `;
 
 const MUTATION_UPDATE_USER_INFO = gql`
-  mutation UpdateUser($email: String!, $firstName: String, $lastName: String) {
-    updateUser(email: $email, firstName: $firstName, lastName: $lastName) {
+  mutation UpdateUser($firstName: String, $lastName: String) {
+    updateUser(firstName: $firstName, lastName: $lastName) {
       success
       errors
     }
@@ -73,12 +72,10 @@ const ProfilePage = () => {
     initialValues: {
       firstName: "",
       lastName: "",
-      email: "",
     },
     validationSchema: Yup.object({
       firstName: Yup.string().max(20).required("Required"),
       lastName: Yup.string().max(20).required("Required"),
-      email: Yup.string().email("Invalid Email Provided").required("Required"),
     }),
     onSubmit: (values) => {
       updateUser({
@@ -265,52 +262,6 @@ const ProfilePage = () => {
                           {formik.errors.lastName}
                         </p>
                       ) : null}
-                    </div>
-                    <div className="col-span-6 sm:col-span-4">
-                      <div>
-                        <label
-                          htmlFor="email"
-                          className="block text-sm font-medium leading-5 text-gray-700"
-                        >
-                          Email address
-                        </label>
-                        <div className="mt-1 relative rounded-md shadow-sm">
-                          <input
-                            id="email"
-                            value={formik.values.email}
-                            onChange={formik.handleChange}
-                            onBlur={formik.handleBlur}
-                            className={`mt-1 form-input block w-full py-2 px-3 border ${
-                              formik.touched.email && formik.errors.email
-                                ? `border-red-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-red focus:border-red-300 text-red-900 placeholder-red-300`
-                                : `border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300`
-                            } transition duration-150 ease-in-out sm:text-sm sm:leading-5`}
-                          />
-                          {formik.touched.email && formik.errors.email ? (
-                            <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
-                              <svg
-                                className="h-5 w-5 text-red-500"
-                                fill="currentColor"
-                                viewBox="0 0 20 20"
-                              >
-                                <path
-                                  fillRule="evenodd"
-                                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
-                                  clipRule="evenodd"
-                                />
-                              </svg>
-                            </div>
-                          ) : null}
-                        </div>
-                        {formik.touched.email && formik.errors.email ? (
-                          <p
-                            className="mt-2 text-sm text-red-600"
-                            id="email-error"
-                          >
-                            {formik.errors.email}
-                          </p>
-                        ) : null}
-                      </div>
                     </div>
                   </div>
                 </div>
