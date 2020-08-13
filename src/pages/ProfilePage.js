51e3a27fd58806077144e2e1bc2b09d1f93b4542
Aditya <dtrg21@gmail.com>
2020-08-13 20:00:10 +0530

    Add connect github button on profile page

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -9,6 +9,7 @@ import DeleteConfirm from "../components/DeleteConfirm";
 import Notification from "../components/Notification";
 import Portal from "../Portal";
 import PopupWindow from "../helpers/PopupWindow";
+import GitHubLogin from "react-github-login";
 
 const GET_CURRENT_USER_QUERY = gql`
   query {
@@ -44,6 +45,15 @@ const MUTATION_SET_USER_PASSWORD = gql`
       success
       errors
       url
+    }
+  }
+`;
+
+const MUTATION_ADD_GITHUB_AUTH = gql`
+  mutation($code: String!) {
+    addGithubAuth(code: $code) {
+      success
+      errors
     }
   }
 `;
@@ -148,6 +158,22 @@ const ProfilePage = () => {
     },
     onError: (_err) => history.push("/error"),
   });
+
+  const [addGithubAuth, { loading: ghLoading }] = useMutation(
+    MUTATION_ADD_GITHUB_AUTH,
+    {
+      onCompleted(data) {
+        if (data.addGithubAuth.success) {
+          setAuthMethods({
+            ...authMethods,
+            github: true,
+          });
+          setNotificationString("GitHub Connected Successfully!");
+          setNotification(true);
+        }
+      },
+    }
+  );
 
   if (loading) {
     return <>Loading...</>;
@@ -316,6 +342,68 @@ const ProfilePage = () => {
           </div>
         </div>
       </div>
+
+      {!authMethods.github && (
+        <>
+          <div className="hidden sm:block">
+            <div className="py-5">
+              <div className="border-t border-gray-200"> </div>
+            </div>
+          </div>
+
+          <div className="mt-10 sm:mt-0">
+            <div className="md:grid md:grid-cols-3 md:gap-6">
+              <div></div>
+              <div className="mt-5 md:mt-0 md:col-span-2">
+                <div className="bg-white shadow sm:rounded-lg">
+                  <div className="px-4 py-5 sm:p-6">
+                    <h3 className="text-lg leading-6 font-medium text-gray-900">
+                      Add GitHub Authentication
+                    </h3>
+                    <div className="mt-2 max-w-xl text-sm leading-5 text-gray-500">
+                      <p>
+                        Login to Hyperlog easily by using GitHub Authentication,
+                        but to do that, connect your GitHub account here first.
+                      </p>
+                    </div>
+                    <div className="mt-5">
+                      <GitHubLogin
+                        clientId="42782b0ad960d7bae699"
+                        redirectUri=""
+                        className="inline-flex items-center px-4 py-2 border border-gray-300 text-base leading-6 font-medium rounded-md text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150"
+                        onSuccess={(data) => {
+                          addGithubAuth({ variables: { code: data.code } });
+                        }}
+                        onFailure={(e) => console.log(e)}
+                        children={
+                          ghLoading ? (
+                            <PulseLoader size="10px" />
+                          ) : (
+                            <>
+                              <svg
+                                className="-ml-1 mr-3 w-5 h-5"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                              >
+                                <path
+                                  fillRule="evenodd"
+                                  d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z"
+                                  clipRule="evenodd"
+                                />
+                              </svg>
+                              Connect Github
+                            </>
+                          )
+                        }
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
 
       {!authMethods.password && (
         <>
