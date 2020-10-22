a51164e15dbe65ca32f0f9090c3973fdb6d2c513
Aditya <dtrg21@gmail.com>
2020-10-22 20:42:55 +0530

    Add ignore classes list in different file

--- postcss.config.js
+++ postcss.config.js
@@ -9,36 +9,6 @@ const purgecss = require("@fullhuman/postcss-purgecss")({
 
   // Include any special characters you're using in this regular expression
   defaultExtractor: (content) => content.match(/[\w-/.:]+(?<!:)/g) || [],
-
-  safelist: [
-    "from-teal-100",
-    "to-teal-100",
-    "bg-teal-400",
-    "from-green-100",
-    "to-green-100",
-    "bg-green-400",
-    "from-yellow-100",
-    "to-yellow-100",
-    "bg-yellow-400",
-    "from-red-100",
-    "to-red-100",
-    "bg-red-400",
-    "from-orange-100",
-    "to-orange-100",
-    "bg-orange-400",
-    "from-blue-100",
-    "to-blue-100",
-    "bg-blue-400",
-    "from-indigo-100",
-    "to-indigo-100",
-    "bg-indigo-400",
-    "from-purple-100",
-    "to-purple-100",
-    "bg-purple-400",
-    "from-pink-100",
-    "to-pink-100",
-    "bg-pink-400",
-  ],
 });
 
 module.exports = {
---
+++ src/ignoreClassesList.js
@@ -0,0 +1,83 @@
+import React from "react";
+
+const ignoreList = () => {
+  return (
+    <div
+      className="
+  from-teal-100
+      to-teal-200
+      bg-teal-600
+      hover:bg-teal-500
+      focus:border-teal-700
+      focus:shadow-outline-teal
+      active:bg-teal-700
+      bg-teal-400
+      from-green-100
+      to-green-200
+      bg-green-600
+      hover:bg-green-500
+      focus:border-green-700
+      focus:shadow-outline-green
+      active:bg-green-700
+      bg-green-400
+      from-yellow-100
+      to-yellow-200
+      bg-yellow-600
+      hover:bg-yellow-500
+      focus:border-yellow-700
+      focus:shadow-outline-yellow
+      active:bg-yellow-700
+      bg-yellow-400
+      from-red-100
+      to-red-200
+      bg-red-600
+      hover:bg-red-500
+      focus:border-red-700
+      focus:shadow-outline-red
+      active:bg-red-700
+      bg-red-400
+      from-orange-100
+      to-orange-200
+      bg-orange-600
+      hover:bg-orange-500
+      focus:border-orange-700
+      focus:shadow-outline-orange
+      active:bg-orange-700
+      bg-orange-400
+      from-blue-100
+      to-blue-200
+      bg-blue-600
+      hover:bg-blue-500
+      focus:border-blue-700
+      focus:shadow-outline-blue
+      active:bg-blue-700
+      bg-blue-400
+      from-indigo-100
+      to-indigo-200
+      bg-indigo-600
+      hover:bg-indigo-500
+      focus:border-indigo-700
+      focus:shadow-outline-indigo
+      active:bg-indigo-700
+      bg-indigo-400
+      from-purple-100
+      to-purple-200
+      bg-purple-600
+      hover:bg-purple-500
+      focus:border-purple-700
+      focus:shadow-outline-purple
+      active:bg-purple-700
+      bg-purple-400
+      from-pink-100
+      to-pink-200
+      bg-pink-600
+      hover:bg-pink-500
+      focus:border-pink-700
+      focus:shadow-outline-pink
+      active:bg-pink-700
+      bg-pink-400"
+    ></div>
+  );
+};
+
+export default ignoreList;
