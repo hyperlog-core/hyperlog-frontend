6c28119a666566765cde17d166fffa228e9a6c35
Aditya Giri <dtrg21@gmail.com>
2021-01-26 21:32:04 +0530

    Remove Customization Tab

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -6,7 +6,6 @@ import { BeatLoader } from "react-spinners";
 import SetUserInfoStep from "../components/SetUserInfoStep";
 import ProfileInfo from "../components/ProfileInfo";
 import ComingSoon from "../components/ComingSoon";
-import CustomizationsPage from "../components/UserSiteSettingsPage.js";
 import MessageSection from "../components/MessageSection";
 
 const GET_USER_POLL = gql`
@@ -317,40 +316,6 @@ const DashboardPage = () => {
                   </span>
                 )}
               </button>
-              <button
-                onClick={() => setSelectedItem("customizations")}
-                className={
-                  selectedItem === "customizations"
-                    ? SELECTED_MENU_ITEM
-                    : NORMAL_MENU_ITEM
-                }
-              >
-                <svg
-                  className={
-                    selectedItem === "customizations"
-                      ? SELECTED_MENU_ICON
-                      : NORMAL_MENU_ICON
-                  }
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-                  />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                  />
-                </svg>
-                <span className="truncate">Customizations</span>
-              </button>
             </nav>
           </div>
           <div className="mt-5 md:mt-0 md:col-span-3">
@@ -365,9 +330,6 @@ const DashboardPage = () => {
               <ComingSoon username={user.username} />
             )}
             {selectedItem === "messages" && <MessageSection />}
-            {selectedItem === "customizations" && (
-              <CustomizationsPage theme={user.themeCode} />
-            )}
           </div>
         </div>
       </UserLayout>
